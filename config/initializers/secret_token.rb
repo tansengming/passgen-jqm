5a899f2884a4a619358cac8a03686fd51accfb15
SengMing Tan <tansengming@gmail.com>
2012-03-15 10:43:12 +1100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JqmPasswords::Application.config.secret_token = 'e8c1a32b92c6b525cc0644734bf1a6315380d549d9efd42e50a8b2fff548f4dfe9b8a0d505ebf019620e6c88c8457abd63ba8d373758c1b2d53c3a4b87cf2443'
