210edf94db1cbcbe933590ac97c24b13e7168347
SengMing Tan <tansengming@gmail.com>
2012-03-30 17:53:48 +0800

    adds GA

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,4 @@
 //= require jquery_ujs
 //= require jquery.mobile
 //= require md5
+//= require async_analytics
---
+++ app/assets/javascripts/async_analytics.js
@@ -0,0 +1,6 @@
+var _gaq=[['_setAccount','UA-30465523-1'],['_trackPageview']];
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
